6e8a95dd22b001118c7deff4e0a05369f8d354ba
abdullahfayyaz <120156998+abdullahfayyaz@users.noreply.github.com>
2025-07-26 16:46:16 +0500

    Update settings.py

--- ISProject/ISProject/settings.py
+++ ISProject/ISProject/settings.py
@@ -139,11 +139,3 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 MEDIA_URL = '/encrypted/'  # URL to serve media files (e.g., http://127.0.0.1:8000/media/)
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  # Local directory for uploaded files (e.g., /path/to/project/media/)
-
-
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_PORT = 587
-# EMAIL_USE_TLS = True
-# EMAIL_HOST_USER = 'innoxentali261271@gmail.com'
-# EMAIL_HOST_PASSWORD = 'dzzd hxhy ipwh nppb' 
